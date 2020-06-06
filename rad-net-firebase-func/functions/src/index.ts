9dec599fd286eca9820c71dbc22cf440f4ae1a46
Gary Grumbley <ggrumbley@gmail.com>
2020-06-06 15:01:24 -0700

    refactor: add express

--- rad-net-firebase-func/functions/src/index.ts
+++ rad-net-firebase-func/functions/src/index.ts
@@ -1,26 +1,41 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-admin.initializeApp();
+const config = {
+  apiKey: 'AIzaSyAhG_tcWTU2kGFFgKVLgNr5v11ikSM6m9k',
+  authDomain: 'rad-net.firebaseapp.com',
+  databaseURL: 'https://rad-net.firebaseio.com',
+  projectId: 'rad-net',
+  storageBucket: 'rad-net.appspot.com',
+  messagingSenderId: '324902657536',
+  appId: '1:324902657536:web:b96e114de628fa92cf82e5',
+};
+
+admin.initializeApp(config);
 
 interface Doot {
+  dootId: string;
   body: string;
-  createdAt: Date;
+  createdAt: string;
   userName: string;
 }
 
-export const getDoots = functions.https.onRequest((req, res) => {
+app.get('/doots', (req, res) => {
   admin
     .firestore()
-    .collection("doots")
+    .collection('doots')
+    .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
       let doots: Doot[] = [];
       data.forEach((doc) => {
-        doots.push(doc.data() as Doot);
+        doots.push({
+          ...(doc.data() as Doot),
+          dootId: doc.id,
+        });
       });
 
       return res.json(doots);
@@ -28,25 +43,24 @@ export const getDoots = functions.https.onRequest((req, res) => {
     .catch((err) => console.error(err));
 });
 
-export const createDoot = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST")
-    return res.status(400).json({ error: "Method not allowed" });
-
+app.post('/doot', (req, res) => {
   const newDoot = {
     body: req.body.body,
     userName: req.body.userName,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
 
   admin
     .firestore()
-    .collection("doots")
+    .collection('doots')
     .add(newDoot)
     .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch((err) => {
-      res.status(500).json({ error: "something blew up" });
+      res.status(500).json({ error: 'something blew up' });
       console.error(err);
     });
 });
+
+export const api = functions.https.onRequest(app);
