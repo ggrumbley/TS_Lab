b4f87fe27601cdb6ba07e1c9b26f0b3d4008ab96
gary <Gary_Grumbley@intuit.com>
2020-07-09 17:17:28 -0700

    feat(survey-app): add survey routes and class

--- Deno_Land/survey-app/server.ts
+++ Deno_Land/survey-app/server.ts
@@ -1,8 +1,15 @@
-import { Application, Router } from 'https://deno.land/x/oak@v5.3.1/mod.ts'
+// denon run --allow-net server.ts
+import { Application } from "./deps.ts";
+import router from "./router.ts";
 
 const app = new Application();
-const router = new Router();
 
-router.get('/', () => {
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-})
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  const protocol = secure ? "https://" : "http://";
+  console.log(`Listening on ${protocol}${hostname || "localhost"}:${port}`);
+});
+
+await app.listen({ port: 8000 });
